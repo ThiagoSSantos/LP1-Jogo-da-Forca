d41f4ed768efd2046def6090bcba06ac4de51e7b
ThiagoSSantos <thiago_ssantos@hotmail.com.br>
2015-10-21 10:43:49 -0200

    Create vetor.c

---
+++ vetor.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<strlen>
+int main()
+{
+    char c = 'A';
+    char s1[100]="A";// A, 0000 0000
+    char s2[100] = {  'A', '\0'  }; //A, 0000 0000
+    char s3[100] = {  'A', 'b','e','l','h','a','\0'};
+    char s4[100]="Abelha"
+    printf("%c\n", c);
+    printf("%s\n", s1);
+    printf("%s\n", s2);
+    printf("%s\n", s3);
+    printf("%s\n", s4);
+
+    printf("%zu\n", strlen(s1));
+    printf("%zu\n", strlen(s2));
+    printf("%zu\n", strlen(s3));
+    printf("%zu\n", strlen(s4));
+    
+	return 0;
+
+}
